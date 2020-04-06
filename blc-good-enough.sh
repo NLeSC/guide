12bd5cd63fbb45a09f9157552fee2691823bed8f
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-04-06 12:50:22 +0200

    Switched from broken-link-checker to liche
    To check links in Markdown files instead of a hosted guide

--- blc-good-enough.sh
+++ blc-good-enough.sh
@@ -8,18 +8,18 @@ broken_max="$1"
 # run the link checker
 echo "travis_fold:start:blc"
 echo "Checking for any broken links..."
-node_modules/.bin/blc --recursive --ordered http://localhost:4000 2>/dev/null | tee stdout.txt
+docker run -v $PWD:/docs peterevans/liche -t 60 -c 16 -d /docs -r /docs >/dev/null 2| tee stdout.txt
 echo "travis_fold:end:blc"
 
 # get the actual number of broken links
-broken=$(cat stdout.txt | tail -n 2 | head -n 1 | grep --word-regexp --only-matching --extended-regexp --regexp='[0-9]{1,}' | tail -n 1)
+broken=$(grep ERROR stdout.txt | wc -l)
 
 echo
 echo "-------------------------------------------------------------------------------------------"
 echo "--                             BROKEN LINK CHECKER: SUMMARY                              --"
 echo "-------------------------------------------------------------------------------------------"
 echo
-cat stdout.txt | grep --invert-match --regexp='^.*[^BR]OK[^EN].*$' | grep --before-context=1 --after-context=1 --regexp="^.*BROKEN.*$"
+cat stdout.txt
 if [ "$broken" -gt "$broken_max" ]; then
     echo "Number of broken links (${broken}) exceeds maximum allowed number (${broken_max})." >&2
     exit 1
